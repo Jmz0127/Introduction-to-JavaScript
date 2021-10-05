ac2c9010833545651d1b869bffad39c925b6370f
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-10-05 19:07:37 -0400

    got task 4 working correctly

--- index.js
+++ index.js
@@ -159,9 +159,35 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
-function game(user, computer) {
-	/*add your code here*/
+let computer = Math.random();
+
+if (computer <= 0.34) {
+	computer = 'rock';
+} else if (computer <= 0.67) {
+	computer = 'paper';
+} else if (computer > 0.67) {
+	computer = 'scissors';
 }
+
+function game(user, computer) {
+	if (computer === user) {
+		return `it's a tie`;
+	} else if (user === 'rock' && computer === 'scissors') {
+		return `you win!`;
+	} else if (user === 'rock' && computer === 'paper') {
+		return `you lose!`;
+	} else if (user === 'paper' && computer === 'scissors') {
+		return `you lose!`;
+	} else if (user === 'paper' && computer === 'rock') {
+		return `you win!`;
+	} else if (user === 'scissors' && computer === 'paper') {
+		return `you win!`;
+	} else if (user === 'scissors' && computer === 'rock') {
+		return `you lose!`;
+	}
+}
+
+console.log(game('rock', computer));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
